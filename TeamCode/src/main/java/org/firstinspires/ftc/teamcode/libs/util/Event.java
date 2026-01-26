75cb260882c9657c1a89ebf4d480e3f3654ad742
Gavin Farrell <gavbfarrell.041409@outlook.com>
2026-01-26 10:25:57 -0500

    Made changes to scheduler and event

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/util/Event.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/util/Event.java
@@ -1,10 +1,55 @@
 package org.firstinspires.ftc.teamcode.libs.util;
 
+import java.util.Random;
+
 public class Event {
 
-    private final String ID;
+    private String ID;
 
     private final Runnable event;
 
     private final Stopwatch stopwatch;
+
+    public Event(long millis, String ID, Runnable event){
+
+        this.ID = ID;
+        this.event = event;
+        this.stopwatch = new Stopwatch(millis);
+
+    }
+
+    public void run(){
+
+        event.run();
+
+    }
+
+    public static String randomID(int length){
+
+        final String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < length; i++){
+            final int randomInt = random.nextInt(62);
+            builder.append(chars.charAt(randomInt));
+        }
+
+        return builder.toString();
+
+    }
+
+    public String getID() {
+        return ID;
+    }
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public Stopwatch getStopwatch(){
+
+        return stopwatch;
+    }
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/util/Scheduler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/util/Scheduler.java
@@ -6,4 +6,68 @@ import java.util.List;
 public class Scheduler {
 
     private final List<Event> events = new ArrayList<>();
+    private final List<String> IDs = new ArrayList<>();
+
+    public Scheduler(){
+
+    }
+
+    public Event schedule_event(long millis, Runnable runnable){
+
+        //creates new event based on parameter
+
+        String ID = Event.randomID(5);
+
+        while(IDs.contains(ID)){
+
+            ID = Event.randomID(5);
+
+        }
+
+        Event event = new Event(millis, ID, runnable);
+
+        events.add(event);
+
+        IDs.add(event.getID());
+
+        return event;
+
+    }
+
+    public Event set_event(Long millis, String id, Runnable runnable) {
+
+        Event event = get_event(id);
+
+        if (event != null) {
+            cancel(id);
+            event = new Event(millis == null ? event.getStopwatch().remainingMillis() : millis,
+                    id, runnable == null ? event.event : runnable);
+            return schedule_event(event);
+        }
+        return schedule(millis, id, runnable);
+    }
+
+    public Event get_event(String ID) {
+
+        if (IDs.contains(ID)) {
+
+            return events.get(IDs.indexOf(ID));
+
+        }
+
+        return null;
+
+    }
+
+    public void cancel(String ID) {
+
+        if (IDs.contains(ID)) {
+
+            events.remove(events.get(IDs.indexOf(ID)));
+            IDs.remove(ID);
+
+        }
+
+    }
+
 }
